c4c6ab97d452026a73220e5930ef69837efcc2a7
Paddy McCann <pgm5@st-andrews.ac.uk>
2019-06-20 20:02:01 +0100

    OAuth Token requirements

--- pescar/settings.py
+++ pescar/settings.py
@@ -44,8 +44,7 @@ INSTALLED_APPS = [
 AUTHENTICATION_BACKENDS = [
     'oauth2_provider.backends.OAuth2Backend',
     # Uncomment following if you want to access the admin
-    'django.contrib.auth.backends.ModelBackend'
-    '...',
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 MIDDLEWARE = [
