2d6543f183d39dd03f2bfa686986e4a434a82391
Paddy McCann <pgm5@st-andrews.ac.uk>
2019-06-23 14:51:23 +0100

    Debugging POST requests

--- pescar/views.py
+++ pescar/views.py
@@ -23,7 +23,8 @@ class ApiEndpoint(ProtectedResourceView):
             else:
                 user_id = users[0][0]
             sys.stderr.write('User ID: {}'.format(user_id))
-            sys.stderr.write('Data: \n {}'.format(request.body.decode('utf-8')))
-            cursor.execute("INSERT INTO trips (user_id, data) VALUES ('{}', '{}')".format(user_id, request.body.decode('utf-8')))
+            body = request.body.decode('utf-8')
+            sys.stderr.write('Data: \n {}'.format(body))
+            cursor.execute("INSERT INTO trips (user_id, data) VALUES ('{}', '{}')".format(user_id, body))
             return HttpResponse("Success!", 200)
 
