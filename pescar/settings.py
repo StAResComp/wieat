48662978f38b7e285cc17a8a9f3d315e01ed3829
Paddy McCann <pgm5@st-andrews.ac.uk>
2019-08-14 16:35:22 +0100

    Finished debugging

--- pescar/settings.py
+++ pescar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=y!hj8x-l_*=g+^xhsvnfp2%rhxs3t$g*+y6!gowg(5t+b%)uu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','arts.st-andrews.ac.uk','rescomp-dev-1.st-andrews.ac.uk']
 
