dc2240ae6796cde008605bdb3c4b43e10ad8c10a
Paddy McCann <pgm5@st-andrews.ac.uk>
2019-08-19 15:37:42 +0100

    Implementing logging

--- pescar/settings.py
+++ pescar/settings.py
@@ -174,7 +174,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, '..', 'debug.log',
+            'filename': os.path.join(BASE_DIR, '..', 'debug.log'),
         },
     },
     'loggers': {
